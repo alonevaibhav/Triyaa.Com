d068c1ba949771e45dadc2fb5bec8a5a2c824d38
alonevaibhav <91304172+alonevaibhav@users.noreply.github.com>
2024-12-20 12:37:00 +0530

    First commite

---
+++ android/app/src/main/kotlin/com/triyaa/triyaa_com/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.triyaa.triyaa_com
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/View/WelcomePage/welcome_page.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Text("hello"),
+    );
+  }
+}
