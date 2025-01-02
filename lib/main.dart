d9e009321ca32858033a39365357e2769e7cb717
alonevaibhav <91304172+alonevaibhav@users.noreply.github.com>
2025-01-02 11:03:33 +0530

    commite

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,9 @@ import 'package:get/get.dart';
 import 'package:triyaa_com/Helper/colors.dart';
 import 'package:triyaa_com/View/Dashboard/ButtomNavBar/button_nav_bar.dart';
 import 'package:triyaa_com/View/WelcomePage/welcome_page.dart';
-import 'package:geolocator/geolocator.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await requestLocationPermission(); // Request location permission at app start
+void main() {
   runApp(const MyApp());
-}
-
-Future<void> requestLocationPermission() async {
-  bool serviceEnabled;
-  LocationPermission permission;
-
-  // Check if location services are enabled
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    // Location services are not enabled, request user to enable them
-    throw Exception('Location services are disabled.');
-  }
-
-  // Check and request location permission
-  permission = await Geolocator.checkPermission();
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
-      // Permission denied, return
-      throw Exception('Location permission denied.');
-    }
-  }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Permissions are denied forever
-    throw Exception('Location permissions are permanently denied.');
-  }
 }
 
 class MyApp extends StatelessWidget {
@@ -51,8 +21,7 @@ class MyApp extends StatelessWidget {
         hintColor: hintColor,
         colorScheme: ColorScheme.fromSeed(seedColor: backgroundColor),
       ),
-      home:
-      WelcomePage(),
+      home: HomeScreen(),
       // HomeScreen(),
     );
   }
